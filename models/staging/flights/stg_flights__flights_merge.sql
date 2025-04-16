20d31ec56432ac6b9fbd69e4a16e4482df806ed0
Mikhail139 <misha14292@gmail.com>
2025-04-16 16:14:07 +0500

    Добавление инкрементации и моделей

---
+++ models/staging/flights/stg_flights__flights_merge.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized = 'incremental',
+        incremental_strategy = 'merge',
+        unique_key = ['flight_id'],
+        on_schema_change = 'sync_all_columns'
+    )
+}}
+select
+flight_id,
+flight_no,
+scheduled_departure,
+scheduled_arrival,
+departure_airport,
+arrival_airport,
+status,
+aircraft_code,
+actual_departure,
+actual_arrival
+from 
+  {{ source('demo_src', 'flights') }}
+{% if is_incremental() %}
+where
+    flight_id > (select max(flight_id) from {{ source('demo_src', 'flights') }})
+{% endif %}
+    
